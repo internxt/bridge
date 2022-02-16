bc12733e2f4d44a53534a7f0b94ca2d8780b2c32
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-02-16 11:45:45 +0100

    refactor(bin): delete empty mirrors command

--- bin/delete-empty-mirrors.js
+++ bin/delete-empty-mirrors.js
@@ -5,145 +5,135 @@
 const program = require('commander');
 const Storage = require('storj-service-storage-models');
 const mongoose = require('mongoose');
+const { eachLimit } = require('async');
 const Config = require('../lib/config');
 
 program.version('0.0.1')
   .option('-c, --config <path_to_config_file>', 'path to the config file')
   .option('-u, --mongourl <mongo connection string>', 'mongo url connection string')
+  .option('-m, --lastMirror <last deleted mirror>', 'last deleted mirror from where to continue')
   .parse(process.argv);
 
-
-function deleteEmptyMirrors() {
-  if (!process.env.NODE_ENV) {
-    throw new Error('NODE_ENV is not set');
-  }
-  try {
-    let mongourl;
-    let deleteCount = 0;
-
-    if (program.mongourl) {
-      mongourl = program.mongourl;
-    }
-
-    const config = new Config(process.env.NODE_ENV, program.config);
-    const storage = new Storage(
-      mongourl || config.storage.mongoUrl,
-      config.storage.mongoOpts
-    );
-
-    const Mirror = storage.models.Mirror;
-    const Shard = storage.models.Shard;
-
-    const cursor = Mirror.find()
-      .sort({
-        '_id': 1
-      })
-      .cursor();
-
-    let chunkOfMirrors = [];
-    const chunkSize = 5;
-
-    cursor.on('data', (mirror) => {
-      chunkOfMirrors.push(mirror);
-      if (chunkOfMirrors.length === chunkSize) {
-        cursor.pause();
-      }
-    });
-
-    let idMirrorBeingDeleted;
-
-    const deleteEmptyMirror = (mirror, cb) => {
-      idMirrorBeingDeleted = mirror._id;
-      const { shardHash } = mirror;
-      Shard.findOne({ hash: shardHash }, (err, shard) => {
-        if (err) {
-          cursor.emit('error', err);
-
-          return;
-        }
-        if (!shard) {
-          mirror.remove((err) => {
-            if (err) {
-              cursor.emit('error', err);
-
-              return;
-            }
-            deleteCount += 1;
-            cb();
-          });
-
-          return;
-        }
-        cb();
-      });
-    };
-
-    const deleteEmptyMirrorChunks = (cb) => {
-      if (chunkOfMirrors.length === 0) {
-        cb();
-
-        return;
-      }
-      let remainingItemsProcessed = 0;
-
-      chunkOfMirrors.forEach((mirror, index, remainingChunks) => {
-        deleteEmptyMirror(mirror, () => {
-          remainingItemsProcessed += 1;
-          if (remainingItemsProcessed === remainingChunks.length) {
-            cb();
-          }
-        });
-      });
-    };
-
-    cursor.on('pause', () => {
-      deleteEmptyMirrorChunks(() => {
-        chunkOfMirrors = [];
-        cursor.resume();
-      });
-    });
-
-
-    const logStatus = () => {
-      console.log('Deleted mirrors: ', deleteCount);
-      if (idMirrorBeingDeleted) {
-        console.log('Last mirror checked: ', idMirrorBeingDeleted);
-      }
-    };
-
-    const idInterval = setInterval(logStatus, 4000);
-
-    const stopProcess = () => {
-      clearInterval(idInterval);
-      cursor.close();
-      mongoose.disconnect();
-    };
-
-    const stopProcessAndError = (err) => {
-      console.error('Error processing mirror: ', idMirrorBeingDeleted);
-      console.error('Error: ', err.message);
-      stopProcess();
-    };
-
-    cursor.once('error', stopProcessAndError);
-
-    cursor.once('end', () => {
-      // There might be still some mirrors that are not deleted (the ones that are left before hitting the chunkSize):
-      deleteEmptyMirrorChunks((err) => {
-        if (err) {
-          stopProcessAndError(err);
-
-          return;
-        }
-        console.log('finished');
-        logStatus();
-        stopProcess();
-      });
-    });
-  } catch (err) {
-    console.error('Unexpected error');
-    console.error(err.message);
-  }
+if (!process.env.NODE_ENV) {
+  throw new Error('NODE_ENV is not set');
 }
 
-deleteEmptyMirrors();
+let mongourl;
+let deleteCount = 0;
+let idMirrorBeingDeleted;
+
+if (program.mongourl) {
+  mongourl = program.mongourl;
+}
+
+const config = new Config(process.env.NODE_ENV, program.config);
+const storage = new Storage(
+  mongourl || config.storage.mongoUrl,
+  config.storage.mongoOpts
+);
+
+const { Shard, Mirror } = storage.models;
+
+const logStatus = () => {
+  console.log('Deleted mirrors: ', deleteCount);
+  if (idMirrorBeingDeleted) {
+    console.log('Last mirror checked: ', idMirrorBeingDeleted);
+  }
+};
+
+const loggerInterval = setInterval(logStatus, 4000);
+
+const deleteEmptyMirror = (mirror, cb) => {
+  const { shardHash } = mirror;
+
+  Shard.findOne({ hash: shardHash }, (err, shard) => {
+    if (err) {
+      return cb(err);
+    }
+
+    if (!shard) {
+      return mirror.remove(cb);
+    }
+
+    cb();
+  });
+};
+
+const deleteEmptyMirrorChunks = (mirrors, cb) => {
+  if (mirrors.length === 0) {
+    return cb();
+  }
+
+  eachLimit(mirrors, 1, (mirror, next) => {
+    idMirrorBeingDeleted = mirror._id;
+
+    deleteEmptyMirror(mirror, (err) => {
+      if (err) {
+        next(err);
+      } else {
+        deleteCount += 1;
+        next();
+      }
+    });
+  }, cb);
+};
+
+function deleteEmptyMirrors(cb) {
+  let chunkOfMirrors = [];
+  const chunkSize = 5;
+
+  const cursor = Mirror.find()
+    .sort({
+      '_id': 1
+    })
+    .cursor();
+
+  cursor.once('error', (err) => {
+    // close?
+    cb(err);
+  });
+
+  cursor.once('end', () => {
+    // There might be still some mirrors that are not deleted (the ones that are left before hitting the chunkSize):
+    deleteEmptyMirrorChunks(chunkOfMirrors, (err) => {
+      // TODO: should emit error?
+      if (err) {
+        return cursor.emit('error', err);
+      }
+    });
+    // emit 'end' == close ?
+    cursor.close();
+    // mongoose.disconnect();
+    cb();
+  });
+
+  cursor.on('pause', () => {
+    deleteEmptyMirrorChunks(chunkOfMirrors, (err) => {
+      if (err) {
+        cursor.emit('error', err);
+      } else {
+        cursor.resume();
+      }
+    });
+  });
+
+  cursor.on('data', (mirror) => {
+    chunkOfMirrors.push(mirror);
+    if (chunkOfMirrors.length === chunkSize) {
+      cursor.pause();
+    }
+  });
+}
+
+deleteEmptyMirrors((err) => {
+  clearInterval(loggerInterval);
+
+  let programFinishedMessage = `Program finished. Deleted ${deleteCount} mirrors. Last mirror was ${idMirrorBeingDeleted}.`;
+
+  if (err) {
+    programFinishedMessage += `Error ${err.message || 'Unknown error'}`;
+    console.log(err.stack);
+  }
+
+  console.log(programFinishedMessage);
+});
