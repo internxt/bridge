ece5d5fc45333c29a7325a08d3422e4dafa29e4e
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-04-18 21:55:27 +0200

    feat(contacts): contact model

---
+++ lib/core/contacts/Contact.ts
@@ -0,0 +1,14 @@
+export interface Contact {
+  id: string;
+  address: string;
+  ip: string;
+  port: number;
+  protocol: string;
+  reputation: number;
+  responseTime: number;
+  spaceAvailable: boolean;
+  userAgent: string;
+  lastSeen: Date;
+  lastTimeout: Date;
+  timeoutRate: number;
+}
