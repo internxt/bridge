83d16d282abef9d0c322398712f7a37c7449e96a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-04-18 21:54:07 +0200

    feat(mirrors): mirror entity + repository

---
+++ lib/core/mirrors/MongoDBMirrorsRepository.ts
@@ -0,0 +1,27 @@
+import { Mirror, MirrorWithContact } from "./Mirror";
+import { MirrorsRepository } from "./Repository";
+
+export class MongoDBMirrorsRepository implements MirrorsRepository {
+  constructor(private model: any) {}
+
+  findByShardHashesWithContacts(shardHashes: string[]): Promise<MirrorWithContact[]> {
+    return this.model
+      .find({ shardHash: { $in: shardHashes } })
+      .populate('contact')
+      .exec()
+      .then((mirrors: any) => {
+        return mirrors.map((m: any) => {
+          return {
+            id: m._id,
+            ...m.toObject()
+          };
+        });
+      });
+  }
+
+  async create(data: Omit<Mirror, "id">): Promise<Mirror> {
+    const rawModel = await new this.model({ ...data, created: new Date() }).save();
+  
+    return rawModel.toObject();
+  }
+}
