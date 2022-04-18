b5253ef6fa026aa1f99060ae2e0a042e0b91f50d
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-04-18 21:59:36 +0200

    feat(buckets): completeUpload usecase

--- lib/core/buckets/usecase.ts
+++ lib/core/buckets/usecase.ts
@@ -142,4 +142,103 @@ export class BucketsUsecase {
     return response;
   }
 
+  async completeUpload(
+    userId: string, 
+    bucketId: string, 
+    fileIndex: string, 
+    shards: Pick<Required<Shard>, 'hash' | 'uuid'>[]
+  ): Promise<BucketEntry> {
+    const [bucket, user] = await Promise.all([
+      this.bucketsRepository.findOne({ id: bucketId }),
+      this.usersRepository.findById(userId)
+    ]);
+
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+
+    if (!bucket) {
+      throw new BucketNotFoundError();
+    }
+
+    if (bucket.user !== userId) {
+      throw new BucketForbiddenError();
+    }
+
+    const uploads = await this.uploadsRepository.findByUuids(shards.map(s => s.uuid));
+
+    if (uploads.length !== shards.length) {
+      throw new MissingUploadsError();
+    }
+
+    const bucketEntrySize = uploads.reduce((acumm, upload) => upload.data_size + acumm, 0);
+
+    if (user.maxSpaceBytes < user.totalUsedSpaceBytes + bucketEntrySize) {
+      throw new MaxSpaceUsedError();
+    }
+
+    const shardAndMirrorsCreationPromises = uploads.map(async (upload) => {
+      const { uuid, hash } = shards.find(s => s.uuid === upload.uuid) as Pick<Shard, 'hash' | 'uuid'>;
+      const { data_size, contracts } = upload;
+
+      const newShardCreation = this.shardsRepository.create({
+        hash, 
+        uuid, 
+        size: data_size,
+        contracts: contracts.map(({ nodeID, contract }: any) => ({
+          nodeID, 
+          contract: {
+            ...contract,
+            data_hash: hash
+          }
+        }))
+      });
+
+      const newMirrorsCreation = contracts.map((contract) => {
+        return this.mirrorsRepository.create({
+          isEstablished: true,
+          shardHash: hash,
+          contact: contract.nodeID,
+          contract: { ...contract.contract, data_hash: hash },
+          token: '',
+        });
+      });
+      
+      const [newShard] = await Promise.all([newShardCreation, ...newMirrorsCreation]);
+
+      return newShard;
+    });
+
+    const bucketEntryCreation = this.bucketEntriesRepository.create({
+      name: v4(),
+      bucket: bucketId,
+      index: fileIndex,
+      version: 2,
+      size: bucketEntrySize
+    });
+
+    const [newBucketEntry, ...newShards] = await Promise.all([
+      bucketEntryCreation,
+      ...shardAndMirrorsCreationPromises
+    ]); 
+
+    // TODO: Ensure shards are sorted by index
+    const bucketEntryShardsCreation = newShards.map((shard, index) => {
+      return this.bucketEntryShardsRepository.create({
+        bucketEntry: newBucketEntry.id,
+        shard: shard.id,
+        index
+      });
+    });
+   
+
+    await Promise.all(bucketEntryShardsCreation);    
+    await this.usersRepository.updateTotalUsedSpaceBytes(userId, bucketEntrySize);
+    this.uploadsRepository.deleteManyByUuids(uploads.map(u => u.uuid)).catch((err) => {
+      // TODO: Move to EventBus
+      console.log('completeUpload/uploads-deletion: Failed due to %s. %s', err.message, err.stack);
+    });
+
+    return newBucketEntry;
+  }
 }
