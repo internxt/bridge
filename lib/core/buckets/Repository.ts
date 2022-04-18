a263c6d8e6edab7b5b1dabf88105351f066dc01a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-04-18 21:49:56 +0200

    feat(buckets): find, findOne MongoDB impl

--- lib/core/buckets/Repository.ts
+++ lib/core/buckets/Repository.ts
@@ -1,6 +1,7 @@
 import { Bucket } from "./Bucket";
 
 export interface BucketsRepository {
+  findOne(where: Partial<Bucket>): Promise<Bucket | null>;
   find(where: Partial<Bucket>): Promise<void>;
   removeAll(where: Partial<Bucket>): Promise<void>;
 }
