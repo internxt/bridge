8896f4418a0dca130475786d55fdb738667a6b2b
Fabio <fa.espinosa10@uniandes.edu.co>
2022-05-02 15:00:59 -0500

    fix: use stream paassthrough

--- bin/migrate-users-v1-to-v2.ts
+++ bin/migrate-users-v1-to-v2.ts
@@ -4,9 +4,9 @@ import { v4 as uuidv4 } from 'uuid';
 import { config as loadEnv } from 'dotenv';
 import axios from 'axios';
 import Config from '../lib/config';
-import { pipeline } from 'stream/promises';
 import { iterateOverCursor } from './cleaner/database';
-import { PipelineSource, Stream } from 'stream';
+import { Stream } from 'stream';
+import { IncomingMessage } from 'http';
 
 const Storage = require('storj-service-storage-models') as any;
 
@@ -14,14 +14,14 @@ loadEnv();
 
 // Example:
 // ts-node bin/migrate-v1-to-v2.ts \
-// -e fabioespinosa@hotmail.com \ 
-// -u mongodb://admin:password@localhost:27017/__inxt-network 
+// -e fabioespinosa@hotmail.com \
+// -u mongodb://admin:password@localhost:27017/__inxt-network
 
 program
   .version('0.0.1')
   .option('-c, --config <path_to_config_file>', 'path to the config file')
   .option('-e, --email <user email>', 'user email to migrate')
-  .option('-u, --mongourl <mongo connection string>', 'mongo url connection string')
+  .option('-u, --mongourl <mongo connection string>','mongo url connection string')
   .parse(process.argv);
 
 if (!process.env.NODE_ENV) {
@@ -41,11 +41,21 @@ if (!program.email) {
 }
 
 // TODO: To be decided if by program or ENV variables:
-const S3BucketName = process.env.S3BucketName
-const S3Endpoint = process.env.S3Endpoint
-const S3AccessKeyId = process.env.S3AccessKeyId
-const S3SecretAccessKey = process.env.S3SecretAccessKey
-const S3Region = process.env.S3Region
+const S3BucketName = process.env.S3BucketName as string;
+const S3Endpoint = process.env.S3Endpoint as string;
+const S3AccessKeyId = process.env.S3AccessKeyId as string;
+const S3SecretAccessKey = process.env.S3SecretAccessKey as string;
+const S3Region = process.env.S3Region as string;
+
+if (
+  !S3BucketName ||
+  !S3Endpoint ||
+  !S3AccessKeyId ||
+  !S3SecretAccessKey ||
+  !S3Region
+) {
+  throw new Error('Missing S3 credentials');
+}
 
 const S3Bucket = new AWS.S3({
   endpoint: new AWS.Endpoint(S3Endpoint),
@@ -64,7 +74,6 @@ let checkedBuckets = 0;
 
 const config = new Config(process.env.NODE_ENV, program.config, '') as {
   storage: { mongoUrl: string; mongoOpts: any };
-  nodes: { username: string, password: string };
 };
 const storage = new Storage(
   program.mongourl || config.storage.mongoUrl,
@@ -91,39 +100,48 @@ const logStatus = () => {
 const loggerInterval = setInterval(logStatus, 4000);
 
 const performDownloadUpload = async (uuid: string, downloadUrl: string) => {
-  const response = await axios.get<PipelineSource<ReadableStream>>(downloadUrl, {
-    responseType: 'stream',
-  });
-  const shardReadable = response.data;
+  const { data: shardReadable } = await axios.get<IncomingMessage>(
+    downloadUrl,
+    {
+      responseType: 'stream',
+    }
+  );
 
   const shardWriteable = new Stream.PassThrough();
 
-  S3Bucket.upload({
+  const promise = S3Bucket.upload({
     Bucket: S3BucketName,
     Key: uuid,
-    Body: shardWriteable
-  })
-  return pipeline(shardReadable, shardWriteable);
-}
+    Body: shardWriteable,
+  }).promise();
+
+  shardReadable.pipe(shardWriteable);
+  return promise;
+};
 
 const getDownloadUrl = async (shard: any) => {
-  const mirror = await MirrorModel
-    .findOne({ shardHash: shard.hash })
-    .populate('contact');
-  
+  const { hash } = shard;
+  const mirror = await MirrorModel.findOne({ shardHash: hash }).populate(
+    'contact'
+  );
+
   const { contact } = mirror;
   const { address, port } = contact;
-  
-  const farmerUrl = `http://${address}:${port}/download/link/${shard.hash}`;
+
+  const farmerUrl = `http://${address}:${port}/download/link/${hash}`;
   const farmerRes = await axios.get(farmerUrl);
   const downloadUrl = farmerRes.data.result;
 
   return downloadUrl;
-}
+};
 
-const migrateShard = async (shard: any, index: number, bucketEntry: any): Promise<void> => {
+const migrateShard = async (
+  shard: any,
+  index: number,
+  bucketEntry: any
+): Promise<void> => {
   const uuid = uuidv4();
-  
+
   const downloadUrl = await getDownloadUrl(shard);
 
   await performDownloadUpload(uuid, downloadUrl);
@@ -134,12 +152,12 @@ const migrateShard = async (shard: any, index: number, bucketEntry: any): Promis
   const bucketEntryShard = await BucketEntryShardModel.create({
     bucketEntry: bucketEntry.id,
     shard: shard.id,
-    index
+    index,
   });
-}
+};
 
 const migrateBucketEntry = async (bucketEntry: any) => {
-  if (!bucketEntry.frame) { 
+  if (!bucketEntry.frame) {
     bucketEntry.delete = true;
     return bucketEntry.save();
   }
@@ -151,51 +169,44 @@ const migrateBucketEntry = async (bucketEntry: any) => {
     return bucketEntry.save();
   }
 
-  const promises = shards.map((shard:any, index:number) => migrateShard(shard, index, bucketEntry));
+  const promises = shards.map((shard: any, index: number) =>
+    migrateShard(shard, index, bucketEntry)
+  );
   await Promise.all(promises);
 
   bucketEntry.version = 2;
   await bucketEntry.save();
 };
 
-const migrateBucket = async (bucket:any): Promise<void> => {
+const migrateBucket = async (bucket: any): Promise<void> => {
   const bucketEntries = BucketEntryModel.find({
     $and: [
       { bucket: bucket._id },
       {
-        $or: [
-          { version: { $eq: 1 } },
-          { version: { $exists: false } },
-        ]
-      }
-    ]
+        $or: [{ version: { $eq: 1 } }, { version: { $exists: false } }],
+      },
+    ],
   })
-  .populate({
-    path: 'frame',
-    populate: { 
-      path: 'shards'
-    }
-  })
-  .sort({ _id: 1 })
-  .cursor();
-  
+    .populate({
+      path: 'frame',
+      populate: {
+        path: 'shards',
+      },
+    })
+    .sort({ _id: 1 })
+    .cursor();
+
   await iterateOverCursor(bucketEntries, migrateBucketEntry);
-}
+};
 
 async function migrateUser(userEmail: string): Promise<void> {
   console.log('Migrating user: ', userEmail);
-
-  const bucketsCursor = BucketModel
-    .find({ user: userEmail })
+  const bucketsCursor = BucketModel.find({ user: userEmail })
     .sort({ _id: 1 })
     .cursor();
-  
-  bucketsCursor.once('error', (err:any) => {
-      console.log('fatal error', err)
-    });
 
-  
-  await iterateOverCursor(bucketsCursor, migrateBucket)
+
+  await iterateOverCursor(bucketsCursor, migrateBucket);
 }
 
 function onProgramFinished(err: Error | void) {
