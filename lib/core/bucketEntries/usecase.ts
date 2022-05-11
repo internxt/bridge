a37ab1383c72fbeaa949956c79b5df5dcd93ee31
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-05-12 01:38:59 +0200

    refactor bucketentriesusecase

--- lib/core/bucketEntries/usecase.ts
+++ lib/core/bucketEntries/usecase.ts
@@ -3,16 +3,30 @@ import { BucketEntriesRepository } from './Repository';
 import { BucketNotFoundError, BucketForbiddenError, BucketEntryNotFoundError, BucketEntryFrameNotFoundError } from '../buckets/usecase';
 import { FramesRepository } from '../frames/Repository';
 import { ShardsUsecase } from '../shards/usecase';
+import { BucketEntryShardsRepository } from '../bucketEntryShards/Repository';
+import { ShardsRepository } from '../shards/Repository';
+import { PointersRepository } from '../pointers/Repository';
+
+export class BucketEntryVersionNotFoundError extends Error {
+  constructor() {
+    super('BucketEntryVersion not found');
+
+    Object.setPrototypeOf(this, BucketEntryVersionNotFoundError.prototype);
+  }
+}
 
 export class BucketEntriesUsecase {
   constructor(
     private bucketEntriesRepository: BucketEntriesRepository,
     private bucketsRepository: BucketsRepository,
     private framesRepository: FramesRepository,
+    private bucketEntryShards: BucketEntryShardsRepository,
+    private shardsRepository: ShardsRepository,
+    private pointersRepository: PointersRepository,
     private shardsUsecase: ShardsUsecase,
   ) { }
 
-  async removeFile(bucketId: string, userId: string, fileId: string): Promise<void> {
+  async removeFileFromUser(bucketId: string, fileId: string, userId: string) {
     const bucket = await this.bucketsRepository.findOne({ id: bucketId });
 
     if(!bucket) {
@@ -23,32 +37,55 @@ export class BucketEntriesUsecase {
       throw new BucketForbiddenError();
     }
 
-    const bucketEntry = await this.bucketEntriesRepository.findOneWithFrame({ id: fileId });
+    return this.removeFile(fileId);
+  }
+
+  async removeFileAndValidateBucketExists(bucketId: string, fileId: string) {
+    const bucket = await this.bucketsRepository.findOne({ id: bucketId });
+    
+    if(!bucket) {
+      throw new BucketNotFoundError();
+    }
+
+    return this.removeFile(fileId);
+  }
+
+  removeFile = async (fileId: string): Promise<void> => {
+    const bucketEntry = await this.bucketEntriesRepository.findOne({ id: fileId });
 
     if (!bucketEntry) {
       throw new BucketEntryNotFoundError();
     }
 
-    if (!bucketEntry.frame) {
-      throw new BucketEntryFrameNotFoundError();
+    const version = bucketEntry.version;
+
+    let shardIds: string[];
+
+    if (version === 1) {
+      const frame = await this.framesRepository.findOne({ bucketEntry: bucketEntry.id });
+
+      if (!frame) {
+        console.error('Frame not found for file %s', bucketEntry.id);
+
+        return this.bucketEntriesRepository.deleteByIds([bucketEntry.id]);
+      }
+
+      shardIds = frame.shards;
+
+      await this.framesRepository.deleteByIds([frame.id]);
+      await this.pointersRepository.deleteByIds(shardIds);
+
+    } else if (version === 2 ) {
+      const bucketEntryShards = await this.bucketEntryShards.findByBucketEntry(bucketEntry.id);
+      shardIds = bucketEntryShards.map(bucketEntryShards => bucketEntryShards.shard);
+    } else {
+
+      throw new BucketEntryVersionNotFoundError();
     }
 
-    const frame = await this.framesRepository.findOne({ id: bucketEntry.frame.id });
-
-    if (!frame) {
-      console.error('Frame %s not found for file %s', bucketEntry.frame.id, bucketEntry.id);
-
-      return this.bucketEntriesRepository.deleteByIds([bucketEntry.id]);
-    }
-
-    const version = bucketEntry.version || 1;
-
-    await this.shardsUsecase.deleteShardsByIds(frame.shards, {
-      beforePointerIsDeleted: this.shardsUsecase.enqueueDeleteShardMessage,
-      version,
-    });
-    
-    await this.framesRepository.deleteByIds([frame.id]);
+    const shards = await this.shardsRepository.findByIds(shardIds);
+    const shardHashes = shards.map(shard => shard.hash);
+    await this.shardsUsecase.enqueueDeleteShardMessages(shardHashes, version);
     await this.bucketEntriesRepository.deleteByIds([bucketEntry.id]);
   }
 }
