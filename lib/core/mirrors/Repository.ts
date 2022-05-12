fbeee6352e7e3637603b3af2acef76a9f14f9071
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-05-12 14:22:32 +0200

    feat(mirrors): insert many

--- lib/core/mirrors/Repository.ts
+++ lib/core/mirrors/Repository.ts
@@ -4,4 +4,5 @@ import { Mirror, MirrorWithContact } from "./Mirror";
 export interface MirrorsRepository {
   findByShardHashesWithContacts(shardHashes: Shard['hash'][]): Promise<MirrorWithContact[]>;
   create(data: Omit<Mirror, 'id'>): Promise<Mirror>;
+  insertMany(data: Omit<Mirror, 'id'>[]): Promise<void>;
 }
