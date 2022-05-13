a1ea65e2f77362ae8c73235ce6f8c0ba7095a28b
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-05-13 14:14:22 +0200

    refactor: make deleteByHashes and deleteByUuids

--- lib/core/shards/usecase.ts
+++ lib/core/shards/usecase.ts
@@ -11,7 +11,35 @@ export class ShardsUsecase {
     private networkQueue: NetworkMessageQueue
   ) {}
 
-  enqueueDeleteShardMessages = async(hashes: string[], version: number) => {
+  deleteShardsStorageByUuids = async (uuids: string []) => {
+    for(const uuid of uuids) {
+      const mirrors = await this.mirrorsRepository.findByShardUuidsWithContacts([uuid]);
+      const stillExistentMirrors = mirrors.filter((mirror) => {
+        return mirror.contact && mirror.contact.address && mirror.contact.port;
+      });
+
+      for (const { contact } of stillExistentMirrors) {
+        const { address, port } = contact;
+        
+        const url = `http://${address}:${port}/v2/shards/${uuid}`;
+
+        this.networkQueue.enqueueMessage({
+          type: DELETING_FILE_MESSAGE,
+          payload: { key: uuid, hash: uuid, url }
+        }, (err: Error | undefined) => {
+          if (err) {
+            console.error(
+              'Error enqueuing delete shard uuid %s : %s',
+              uuid, 
+              err.message
+            );
+          }
+        })
+      }
+    }
+  }
+
+  deleteShardsStorageByHashes = async(hashes: string[]) => {
     for(const hash of hashes) {
       const mirrors = await this.mirrorsRepository.findByShardHashesWithContacts([hash]);
       const stillExistentMirrors = mirrors.filter((mirror) => {
@@ -21,21 +49,11 @@ export class ShardsUsecase {
       for (const { contact } of stillExistentMirrors) {
         const { address, port } = contact;
 
-        let url: string;
-        
-        if(version === 1){
-          url = `http://${address}:${port}/shards/${hash}`;
-        }
-        else if (version === 2) {
-          url = `http://${address}:${port}/v2/shards/${hash}`;
-        }
-        else {
-          throw new BucketEntryVersionNotFoundError();
-        }
+        const url = `http://${address}:${port}/shards/${hash}`;
 
         this.networkQueue.enqueueMessage({
           type: DELETING_FILE_MESSAGE,
-          payload: { hash, url }
+          payload: { key: hash, hash, url }
         }, (err: Error | undefined) => {
           if (err) {
             console.error(
