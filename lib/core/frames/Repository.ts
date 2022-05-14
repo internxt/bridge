e836d2a62dba2363b6d359f1398ecf745d93f4c6
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-05-14 19:40:04 +0200

    feat(frames): findByIds

--- lib/core/frames/Repository.ts
+++ lib/core/frames/Repository.ts
@@ -2,6 +2,7 @@ import { User } from "../users/User";
 import { Frame } from "./Frame";
 
 export interface FramesRepository {
+  findByIds(ids: Frame['id'][]): Promise<Frame[]>;
   findOne(where: Partial<Frame>): Promise<Frame | null>;
   getUserUsage(user: User['id']): Promise<{ total: number } | null>;
   removeAll(where: Partial<Frame>, limit?: number): Promise<void>;
