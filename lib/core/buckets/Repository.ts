ad0c5210895964f6d2755f98ddf2c03ca670acdf
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-05-18 21:29:22 +0200

    feat(buckets): find by ids

--- lib/core/buckets/Repository.ts
+++ lib/core/buckets/Repository.ts
@@ -2,6 +2,7 @@ import { Bucket } from "./Bucket";
 
 export interface BucketsRepository {
   findOne(where: Partial<Bucket>): Promise<Bucket | null>;
+  findByIds(ids: Bucket['id'][]): Promise<Bucket[]>;
   find(where: Partial<Bucket>): Promise<void>;
   removeAll(where: Partial<Bucket>): Promise<void>;
 }
