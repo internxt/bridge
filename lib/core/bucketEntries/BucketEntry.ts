ddee3ffd8c9ec25a9c82568b91dd332fe630af2d
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-07-12 11:00:59 +0200

    doc(BucketEntry-model): add deprecation warning

--- lib/core/bucketEntries/BucketEntry.ts
+++ lib/core/bucketEntries/BucketEntry.ts
@@ -3,10 +3,10 @@ import { Frame } from '../frames/Frame';
 
 export interface BucketEntry {
   id: string;
-  // Check if frame can be id or frame itself:
   frame?: Frame['id'];
-  // INCONGRUENCE HERE (filename & name): storj retrieves bucketentry and assigns name to filename (in model)
+  // name is deprecated
   name?: string;
+  // filename is deprecated
   filename?: string;
   index: string;
   bucket: Bucket['id'];
