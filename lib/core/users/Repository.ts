88eb433ccbc623a76a0f6e1f4880c4530acd3b8c
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-06-14 15:32:22 +0200

    feat(user): find by uuid, increment total used space bytes functions

--- lib/core/users/Repository.ts
+++ lib/core/users/Repository.ts
@@ -1,11 +1,13 @@
-import { BasicUser, CreateUserData, User } from "./User";
+import { BasicUser, CreateUserData, User, UserDom } from "./User";
 
 export interface UsersRepository {
   findById(id: User['id']): Promise<User | null>;
+  findByUuid(uuid: User['uuid']): Promise<User | null>;
   findOne(where: Partial<User>): Promise<BasicUser | null>;
   findByIds(ids: User['id'][]): Promise<User[]>;
   create(data: CreateUserData): Promise<BasicUser>;
   addTotalUsedSpaceBytes(id: User['id'], totalUsedSpaceBytes: number): Promise<void>;
+  incrementTotalUsedSpaceBytes(user: UserDom, totalUsedSpaceBytes: number): Promise<void>;
   updateById(id: User['id'], update: Partial<User>): Promise<User | null>;
   updateByUuid(uuid: User['uuid'], update: Partial<User>): Promise<BasicUser | null>;
   removeById(id: User['id']): Promise<void>;
