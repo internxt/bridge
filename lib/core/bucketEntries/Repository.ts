da2cb0ab5d3d6bed46472326b7771a4dc0a14c90
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-06-28 10:16:44 +0200

    feat(bucketentries): findByBucket, count repo fns

--- lib/core/bucketEntries/Repository.ts
+++ lib/core/bucketEntries/Repository.ts
@@ -1,8 +1,11 @@
+import { Bucket } from "../buckets/Bucket";
 import { Frame } from "../frames/Frame";
 import { BucketEntry, BucketEntryWithFrame } from "./BucketEntry";
 
 export interface BucketEntriesRepository {
+  count(where: Partial<BucketEntry>): Promise<number>;
   findOne(where: Partial<BucketEntry>): Promise<BucketEntry | null>;
+  findByBucket(bucketId: Bucket['id'], limit: number, offset: number): Promise<BucketEntry[]>;
   findByIds(ids: BucketEntry['id'][]): Promise<BucketEntry[]>;
   findOneWithFrame(where: Partial<BucketEntry>): Promise<Omit<BucketEntryWithFrame, 'frame'> & { frame?: Frame } | null>;
   findByIdsWithFrames(ids: BucketEntry['id'][]): Promise<(Omit<BucketEntryWithFrame, 'frame'> & { frame?: Frame })[]>;
