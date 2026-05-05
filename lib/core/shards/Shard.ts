9894d8ee484296fed7c8659d0aa67ac6c4e9d49f
Andres <143480783+apsantiso@users.noreply.github.com>
2026-05-05 13:28:39 +0200

    Revert "[IN-145] fix: remove parts from finish endpoint"

--- lib/core/shards/Shard.ts
+++ lib/core/shards/Shard.ts
@@ -25,6 +25,7 @@ export type ShardWithPossibleMultiUpload = Pick<
   'hash' | 'uuid'
 > & {
   UploadId?: string;
+  parts?: { PartNumber: number; ETag: string }[];
 };
 
 export type ShardWithMultiUpload = Required<ShardWithPossibleMultiUpload>;
